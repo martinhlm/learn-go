563f309315766d739e87a06cd2b121f320cee0c0
Martin <mlopez@fadermex.com>
2017-07-04 14:06:10 -0500

    return two strings

--- tourGo/imports.go
+++ tourGo/imports.go
@@ -5,11 +5,18 @@ import (
 	"math"
 )
 
-func add(x, y int) int {
+func add(x, y int) int { 
 	return 	x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
 }
 
 func main() {
 	fmt.Println("Now you have %g problems.", math.Sqrt(7))
 	fmt.Println("add: %g", add(42, 13))
+
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
