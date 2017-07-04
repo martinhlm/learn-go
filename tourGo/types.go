116fa30ce34af972ad79fcc4e7d4ca0667536e35
Martin <mlopez@fadermex.com>
2017-07-04 16:21:50 -0500

    arrays

--- tourGo/types.go
+++ tourGo/types.go
@@ -41,9 +41,21 @@ func structsFunc() {
 	fmt.Println(v1, r, v2, v3)	
 }
 
+func arraysFunc() {
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+}
+
 func more_types() {
 	fmt.Println("pointers")
 	pointers()
 	fmt.Println(Vertex{1, 2})
 	structsFunc()
+	arraysFunc()
 }
