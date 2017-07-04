eeddf790bffa390ef21db6eb2380cc59cf641284
Martin <mlopez@fadermex.com>
2017-07-04 17:59:20 -0500

    slice defaults

--- tourGo/types.go
+++ tourGo/types.go
@@ -54,8 +54,8 @@ func arraysFunc() {
 
 func slicesFunc() {
 	primes := [6]int{2, 3, 5, 7, 11, 13}
-	var s []int = primes[1:4]
-	fmt.Println(s)
+	var p []int = primes[1:4]
+	fmt.Println(p)
 
 	// A slice does not store any data, it just describes 
 	// a section of an underlying array
@@ -71,6 +71,17 @@ func slicesFunc() {
 	b[0] = "XXX"
 	fmt.Println(a,b)
 	fmt.Println(names)
+
+	// defaults
+	s := []int{2, 3, 5, 7, 11, 13}
+	s = s[1:4]
+	fmt.Println(s)
+
+	s = s[:2]
+	fmt.Println(s)
+
+	s = s[1:]
+	fmt.Println(s)
 }
 
 func more_types() {
