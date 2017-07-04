c33e50e80f50314405dfa609f9ade863575058e9
Martin <mlopez@fadermex.com>
2017-07-04 14:02:54 -0500

    add func add

--- tourGo/imports.go
+++ tourGo/imports.go
@@ -5,6 +5,11 @@ import (
 	"math"
 )
 
+func add(x, y int) int {
+	return 	x + y
+}
+
 func main() {
-	fmt.Printf("Now you have %g problems.", math.Sqrt(7))
+	fmt.Println("Now you have %g problems.", math.Sqrt(7))
+	fmt.Println("add: %g", add(42, 13))
 }
