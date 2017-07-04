d8103f5b88fd9ccaadd6f463b8e5274fadbd7238
Martin <mlopez@fadermex.com>
2017-07-04 17:56:38 -0500

    slices like references to array

--- tourGo/types.go
+++ tourGo/types.go
@@ -56,6 +56,21 @@ func slicesFunc() {
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	var s []int = primes[1:4]
 	fmt.Println(s)
+
+	// A slice does not store any data, it just describes 
+	// a section of an underlying array
+	// Changind the elements of a slice modifies the corresponding
+	// elements of its underlying array
+	names := [4]string {
+		"John","Paul","Ringo", "George",
+	}
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a,b)
+	fmt.Println(names)
 }
 
 func more_types() {
