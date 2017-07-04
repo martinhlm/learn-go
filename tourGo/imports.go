590d4f473cead03a9620567c786a65b8a979d30c
Martin <mlopez@fadermex.com>
2017-07-04 14:12:39 -0500

    named return values

--- tourGo/imports.go
+++ tourGo/imports.go
@@ -13,10 +13,18 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
 	fmt.Println("Now you have %g problems.", math.Sqrt(7))
-	fmt.Println("add: %g", add(42, 13))
+	fmt.Println(add(42, 13))
 
 	a, b := swap("hello", "world")
 	fmt.Println(a, b)
+
+	fmt.Println(split(19))
 }
