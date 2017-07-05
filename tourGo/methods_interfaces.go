656e30c13b7eb57be0fc13f22f9de39c15e4ce4d
Martin <mlopez@fadermex.com>
2017-07-05 12:00:26 -0500

    non-struct methods

--- tourGo/methods_interfaces.go
+++ tourGo/methods_interfaces.go
@@ -9,6 +9,8 @@ type Mertex struct {
 	X,Y float64
 }
 
+type MyFloat float64
+
 /* 
 	Go does not have classes. However, you can define methods on types.
 	A method is a function with a special 'receiver' argument.
@@ -22,6 +24,21 @@ func(v Mertex) Abs() float64 {
 func methods() {
 	v := Mertex{3, 4}
 	fmt.Println(v.Abs())
+
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
+}
+
+/*
+	You can declare a method on non-struct types.
+	You can only declare a method with a receiver whose type is defined in the
+	same package as the method.
+*/
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
 }
 
 func methodsInterfaces() {
