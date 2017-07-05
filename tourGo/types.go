053e688da46c996569b1d8895dc48b2575931113
Martin <mlopez@fadermex.com>
2017-07-05 10:20:46 -0500

    appending to slice

--- tourGo/types.go
+++ tourGo/types.go
@@ -84,6 +84,28 @@ func slicesFunc() {
 	fmt.Println(s)
 }
 
+func sliceAppend() {
+	var s []int
+	printSlice(s)
+
+	// append works on nil slices.
+	s = append(s, 0)
+	printSlice(s)
+
+	// The slice grows as needed.
+	s = append(s, 1)
+	printSlice(s)
+
+	// We can add more than one element at a time.
+	s = append(s, 2, 3, 4)
+		printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
+
 func more_types() {
 	fmt.Println("pointers")
 	pointers()
@@ -91,4 +113,5 @@ func more_types() {
 	structsFunc()
 	arraysFunc()
 	slicesFunc()
+	sliceAppend()
 }
