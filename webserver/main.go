c8e1dc138091e78c1227f2710aa393d058eacc5f
Martin <mlopez@fadermex.com>
2017-07-06 10:43:04 -0500

    create simple server

--- webserver/main.go
+++ webserver/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("world")
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, web")
 }
