9039942372720c9b2c32b5bb7233c3233e02ed94
Martin <mlopez@fadermex.com>
2017-07-06 17:57:28 -0500

    set test handler for every case

--- webserver/main_test.go
+++ webserver/main_test.go
@@ -8,22 +8,34 @@ import (
 )
 
 func TestHandler(t *testing.T) {
-	req, err := http.NewRequest(
-		http.MethodGet,
-		"http://localhost:8080/m@golang.org",
-		nil,
-	)
-	if err != nil {
-		t.Fatalf("could not create request: %v", err)
+
+	// table driven test
+	cases := []struct {
+		in, out string
+	}{
+		{"m@golang.org", "gopher m"},
+		{"something", "dear something"},
 	}
 
-	rec := httptest.NewRecorder()
-	handler(rec, req)
+	for _, c := range cases {
+		req, err := http.NewRequest(
+			http.MethodGet,
+			"http://localhost:8080/"+c.in,
+			nil,
+		)
 
-	if rec.Code != http.StatusOK {
-		t.Errorf("Expected status 200, got: %d", rec.Code)
-	}
-	if !strings.Contains(rec.Body.String(), "gopher m") {
-		t.Errorf("unexpected body in response: %q", rec.Body.String())
+		if err != nil {
+			t.Fatalf("could not create request: %v", err)
+		}
+
+		rec := httptest.NewRecorder()
+		handler(rec, req)
+
+		if rec.Code != http.StatusOK {
+			t.Errorf("Expected status 200, got: %d", rec.Code)
+		}
+		if !strings.Contains(rec.Body.String(), c.out) {
+			t.Errorf("unexpected body in response: %q", rec.Body.String())
+		}
 	}
 }
