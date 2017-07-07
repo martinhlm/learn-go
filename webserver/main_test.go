59444af5e7e3acc0942dff8f3575c7adc9ccc7ed
Martin <mlopez@fadermex.com>
2017-07-07 09:42:01 -0500

    benchmarking

--- webserver/main_test.go
+++ webserver/main_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestHandler(t *testing.T) {
+/*func TestHandler(t *testing.T) {
 
 	// table driven test
 	cases := []struct {
@@ -36,6 +36,31 @@ func TestHandler(t *testing.T) {
 		}
 		if !strings.Contains(rec.Body.String(), c.out) {
 			t.Errorf("unexpected body in response: %q", rec.Body.String())
+		}
+	}
+}*/
+
+func BenchmarkHandler(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		req, err := http.NewRequest(
+			http.MethodGet,
+			"http://localhost:8080/m@golang.org",
+			nil,
+		)
+
+		if err != nil {
+			b.Fatalf("could not create request: %v", err)
+		}
+
+		rec := httptest.NewRecorder()
+		handler(rec, req)
+
+		if rec.Code != http.StatusOK {
+			b.Errorf("Expected status 200, got: %d", rec.Code)
+		}
+		if !strings.Contains(rec.Body.String(), "gopher m") {
+			b.Errorf("unexpected body in response: %q", rec.Body.String())
 		}
 	}
 }
