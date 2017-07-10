b129a24ca80b679dd216dcdbf44a5e2bb6b387ea
Martin <mlopez@fadermex.com>
2017-07-10 09:44:09 -0500

    refactoring, create newTaskOrFatal

--- todo/work/task_test.go
+++ todo/work/task_test.go
@@ -2,11 +2,16 @@ package work
 
 import "testing"
 
-func TestNewTask(t *testing.T) {
-	task, err := NewTask("Learn Go")
+func newTaskOrFatal(t *testing.T, title string) *Task {
+	task, err := NewTask(title)
 	if err != nil {
 		t.Fatalf("unexpected error %v", err)
 	}
+	return task
+}
+
+func TestNewTask(t *testing.T) {
+	task := newTaskOrFatal(t, "Learn Go")
 	if task.Title != "Learn Go" {
 		t.Errorf("expected learn Go, got %v", task.Title)
 	}
