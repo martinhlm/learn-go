02523a53849a0ad531a29231f238a17cb8e4dc6e
Martin <mlopez@fadermex.com>
2017-07-11 10:32:18 -0500

    create Add post api request

--- polymer-gopher/step0/posts.go
+++ polymer-gopher/step0/posts.go
@@ -2,12 +2,20 @@ package posts
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/GoogleCloudPlatform/go-endpoints/endpoints"
+
+	"golang.org/x/net/context"
 
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
 )
+
+// define all the endpoints of the posts API
+type PostAPI struct{}
 
 type Post struct {
 	UID      string
@@ -17,12 +25,23 @@ type Post struct {
 	Favorite bool
 }
 
-// func that calls before main
-func init() {
-	http.HandleFunc("/posts", listPosts)
+type Posts struct {
+	Posts []Post
 }
 
-func listPosts(w http.ResponseWriter, r *http.Request) {
+type AddRequest struct {
+	Text     string
+	Username string
+	Avatar   string
+}
+
+// func that calls before main
+func init() {
+	endpoints.HandleHTTP()
+}
+
+// method
+func (PostAPI) list(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 
 	posts := []Post{}
@@ -37,4 +56,19 @@ func listPosts(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("encoding: %v", err)
 	}
+}
+
+func (PostAPI) Add(c context.Context, r *AddRequest) (*Post, error) {
+	p := Post{
+		Text:     r.Text,
+		Username: r.Username,
+		Avatar:   r.Avatar,
+	}
+
+	key := datastore.NewIncompleteKey(c, "Post", nil)
+	key, err := datastore.Put(c, key, &p)
+	if err != nil {
+		return nil, fmt.Errorf("put post: %v", err)
+	}
+	return &p, nil
 }
